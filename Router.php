1376e4b7732a7cff5045aace614b371c9b729b4b
Juan Q <jmquijano@unimayor.edu.co>
2022-10-26 00:01:44 -0500

    Actualización Router

--- Router.php
+++ Router.php
@@ -28,7 +28,11 @@ class Router
 
         $isAdmin = $_SESSION['admin'] ?? null;
 
-        $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
+        if (isset($_SERVER['PATH_INFO'])) {
+            $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
+        } else {
+            $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
+        }
         $method = $_SERVER['REQUEST_METHOD'];
 
         if ($method === 'GET') {
